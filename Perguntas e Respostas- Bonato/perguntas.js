7de1b895af7d5e0b180638ee0f7603a9427d4089
GuilhermBonato <guilhermebonatoforte@gmail.com>
2024-11-30 12:45:59 -0300

    Add files via upload

---
+++ Perguntas e Respostas- Bonato/perguntas.js
@@ -0,0 +1,47 @@
+criaCartao(
+    'Quimica',
+    'Qual é o grupo funcional do metano?',
+    'O grupo funcional do metano é -CH3, conhecido como grupo metil. Isso ocorre porque o metano (CH4) possui um átomo de carbono ligado a três átomos de hidrogênio e um quarto átomo de hidrogênio que pode ser substituído por outro grupo funcional.o'
+)
+
+criaCartao(
+    'Quimica',
+    'Qual é a fórmula estrutural do etano?',
+    'A fórmula estrutural do etano é CH3-CH3. Isso mostra que o etano possui dois átomos de carbono ligados por uma ligação covalente simples e cada átomo de carbono está ligado a três átomos de hidrogênio.'
+)
+
+criaCartao(
+    'Quimica',
+    'Qual é a fórmula química do cloreto de sódio?',
+    'A fórmula química do cloreto de sódio é NaCl. Isso ocorre porque o sódio (Na) perde um elétron para formar um íon positivo (Na+) e o cloro (Cl) ganha um elétron para formar um íon negativo (Cl-), resultando em uma ligação iônica.'
+)
+
+criaCartao(
+    'Quimica',
+    'Qual é o nome do íon SO42-?',
+    'O íon SO42- é chamado de sulfato. Isso ocorre porque o enxofre (S) está ligado a quatro átomos de oxigênio e possui uma carga negativa.'
+)
+
+criaCartao(
+    'Quimica',
+    'Qual é a lei dos gases ideais?',
+    'A lei dos gases ideais é PV = nRT, onde P é a pressão, V é o volume, n é o número de moles, R é a constante dos gases ideais e T é a temperatura em kelvin.'
+)
+
+criaCartao(
+    'Quimica',
+    'Qual é a unidade de medida da entalpia?',
+    'A unidade de medida da entalpia é o joule (J). A entalpia é uma medida da energia total de um sistema.'
+)
+
+criaCartao(
+    'Quimica',
+    'Qual é a reação química entre Zn e HCl?',
+    'A reação química entre Zn (zinc) e HCl (ácido clorídrico) é: Zn + 2HCl → ZnCl2 + H2. Isso ocorre porque o zinc reage com o ácido clorídrico para formar cloreto de zinc e hidrogênio.'
+)
+
+criaCartao(
+    'Quimica',
+    'Qual é a reação química entre NaOH e HCl?',
+    'A reação química entre NaOH (hidróxido de sódio) e HCl é: NaOH + HCl → NaCl + H2O. Isso ocorre porque o hidróxido de sódio reage com o ácido clorídrico para formar cloreto de sódio e água.'
+)
